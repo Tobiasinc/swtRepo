cff2e37f6b08e3e0808c8557ef788bf020e3fca7
Tobias <tobias.klupp@mni.thm.de>
2013-11-27 10:15:27 +0100

    1

---
+++ ViereckGit/src/swt/Viereck.java
@@ -0,0 +1,43 @@
+package swt;
+
+import java.awt.Point;
+
+public class Viereck {
+    private Point a;
+    private Point b;
+    private Point c;
+    private Point d;
+
+    public Viereck(Point a, Point b, Point c, Point d) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+    }
+
+    public boolean isQuadrat() {
+        if ((a.x == b.x) && (a.y == d.y) && (b.y == c.y) && (c.x == d.x)) {
+            int ab = Math.abs(a.y - b.y);
+            int bc = Math.abs(b.x - c.x);
+            int cd = Math.abs(c.y - d.y);
+            int da = Math.abs(d.x - a.x);
+            if (ab == bc && cd == da && bc == cd) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean isParallelogramm() {
+    	if ((a.x == d.x) && (a.y ==d.y) && (b.x == c.x) && (b.y ==c.y)) {
+			if((a.x-b.x) == (d.x-c.x)) {
+				return true;
+			}
+		}
+    	return false;
+    }
+    
+    
+    
+}
+   
